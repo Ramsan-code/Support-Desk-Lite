c995e4d2f562531140782ad32842a7b3c17ef5c0
Ramsan-code <ramsandota@gmail.com>
2026-02-21 15:16:57 +0530

    feat : npm i bcrypt  ,write user schema and encryped hashing password for security

---
+++ src/models/User.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'missing your name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'missing your email'],
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            ' missing your email',
+        ],
+    },
+    password: {
+        type: String,
+        required: [true, 'not match your password'],
+        minlength: 6,
+        select: false,
+    },
+    role: {
+        type: String,
+        enum: ['Customer', 'Agent', 'Admin'],
+        default: 'Customer',
+    },
+}, {
+    timestamps: true,
+});
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model('User', userSchema);
