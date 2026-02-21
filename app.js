993f421529e867f6e3b1e3e28566115570530358
Ramsan-code <ramsandota@gmail.com>
2026-02-21 12:53:19 +0530

    feat: install dotenv  package ,create .env file ,connect mongodb cluster

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // console.log("test");
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 const app = express();
-const PORT = 5000
+const PORT = process.env.PORT || 5000
+
 app.use(express.json());
+const connectDB = async () => {
+     mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDB");
+        };
+
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Hello World");
